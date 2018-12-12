2c3afe70b64b57aa8b5a058ff5cec3bee6ef80f6
wowowo12 <finlinfin@gmail.com>
2018-12-12 18:47:09 +0900

    1812121847

--- public/js/main.js
+++ public/js/main.js
@@ -2,5 +2,5 @@ $(".h_order").click(function(){
     $(this).stop().animate({"bottom":"0"});
 });
 $("header").stop().click(function(){
-    $(".h_order").animate({"bottom":"-27%"});
+    $(".h_order").animate({"bottom":"-255px"});
 });
